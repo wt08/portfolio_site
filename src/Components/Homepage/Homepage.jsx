a93d40df45b3920a14e8671cb30d289807cabcdd
Will Tidwell <willtidwell@Wills-Air.hsd1.sc.comcast.net>
2020-08-24 19:53:10 -0400

    started Projects comp

--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import IconsConnect from '../IconsConnect/IconsConnect'
+import React from "react";
+import IconsConnect from "../IconsConnect/IconsConnect";
+import Projects from "../Projects/Projects";
+import "./Homepage.css";
 
 const Homepage = () => {
-    return (
-        <div>
-            <IconsConnect />
-        </div>
-    )
-}
+  return (
+    <div className="homepage">
+      <IconsConnect />
+      <Projects />
+    </div>
+  );
+};
 
-
-export default Homepage
+export default Homepage;
---
+++ src/Components/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Projects.css";
+
+const Projects = () => {
+  return (
+    <div className="projects">
+      <div className="card1"></div>
+      <div className="card2"></div>
+    </div>
+  );
+};
+
+export default Projects;
