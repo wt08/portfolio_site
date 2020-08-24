d38b264c0b4a1f0d953942fc6e27c9b6a4c71a1a
Will Tidwell <willtidwell@Wills-Air.hsd1.sc.comcast.net>
2020-08-24 16:36:38 -0400

    Tested bootstrap Nav. Will not use, uninstalled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "./Components/Nav/Navigation";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <h1>Hello</h1>
+      
     </div>
   );
 }
