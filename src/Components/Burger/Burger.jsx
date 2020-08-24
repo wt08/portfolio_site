2be1826bc22a78ed552d30c3e4b7f3a52b6470d8
Will Tidwell <willtidwell@Wills-Air.hsd1.sc.comcast.net>
2020-08-24 17:08:40 -0400

    editing Navigation

---
+++ src/Components/Burger/Burger.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './Burger.css'
+
+const Burger = ({menuOpen, setMenuOpen}) => {
+  return (
+    <div
+      onClick={() => setMenuOpen(!menuOpen)}
+      className={`burger ${menuOpen ? "hide" : ""}`}
+    >
+      <div className="burger_line" />
+      <div className="burger_line" />
+      <div className="burger_line" />
+    </div>
+  );
+};
+
+export default Burger;
---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Menu.css";
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <h3>Home</h3>
+      <h3>About</h3>
+    </div>
+  );
+};
+
+export default Menu;
