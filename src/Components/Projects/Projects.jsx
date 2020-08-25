03841a9085b75476777f3a8c27e4586d9bcb9c68
Will Tidwell <willtidwell@Wills-Air.hsd1.sc.comcast.net>
2020-08-25 12:49:18 -0400

    added third project card

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,10 +4,11 @@ import "./Projects.css";
 const Projects = () => {
   return (
     <div className="projects">
-      <h2>Projects:</h2>
+      <h2>Projects</h2>
       <div className="projectsList">
         <div className="card1"></div>
         <div className="card2"></div>
+        <div className="card3"></div>
       </div>
     </div>
   );
