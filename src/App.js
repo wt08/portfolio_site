538c4f705e7a214828a0c7368df0434fe98b4ee9
Will Tidwell <willtidwell@Wills-MacBook-Air.local>
2020-08-28 18:11:52 -0400

    resume comp

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Homepage from "./Components/Homepage/Homepage";
 import { Route } from "react-router-dom";
 import About from "./Components/About/About";
+import Resume from "./Components/Resume/Resume";
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Homepage} />
       <Route path="/about" component={About} />
+      <Route path="/resume" component={Resume} />
     </div>
   );
 }
--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Menu.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimesCircle } from "@fortawesome/free-solid-svg-icons";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Menu = ({ menuOpen, setMenuOpen }) => {
   return (
@@ -13,9 +13,17 @@ const Menu = ({ menuOpen, setMenuOpen }) => {
         onClick={() => setMenuOpen(!menuOpen)}
       />
       <br />
-      <Link to="/"><h3 className="menuItem">Projects</h3></Link>
+      <Link to="/">
+        <h3 className="menuItem">Projects</h3>
+      </Link>
       <br />
-      <Link to="/about"><h3 className="menuItem">About</h3></Link>
+      <Link to="/resume">
+        <h3 className="menuItem">Resume</h3>
+      </Link>
+      <br />
+      <Link to="/about">
+        <h3 className="menuItem">About</h3>
+      </Link>
     </div>
   );
 };
