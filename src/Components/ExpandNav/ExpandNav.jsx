86c6979cde8e6eb7cc5b272f8d6e6e9641e8383e
Will Tidwell <willtidwell@Wills-Air.hsd1.sc.comcast.net>
2020-09-01 15:36:23 -0400

    changed color, font, and basic homepage structure

--- src/Components/ExpandNav/ExpandNav.jsx
+++ src/Components/ExpandNav/ExpandNav.jsx
@@ -6,7 +6,7 @@ const ExpandNav = () => {
   return (
     <div className="expandNav">
       <div>
-        <h1>Will Tidwell</h1>
+        <h1>will tidwell</h1>
         <h4>Software Developer</h4>
       </div>
       <div className="links">
--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -3,15 +3,15 @@ import IconsConnect from "../IconsConnect/IconsConnect";
 import Projects from "../Projects/Projects";
 import "./Homepage.css";
 import Navigation from "../Navigation/Navigation";
-import Footer from '../Footer/Footer'
+import Footer from "../Footer/Footer";
 
 const Homepage = () => {
   return (
     <div className="homepage">
       <Navigation />
       <div className="main">
-        <IconsConnect />
         <Projects />
+        <IconsConnect />
       </div>
       <Footer />
     </div>
